bea5a78d4833353cb0b0c1f0f41e6565486569de
Alyssa Brown <alyssa.brown@live.com>
2019-04-05 05:00:44 +0000

    Automatically backed up by Learn

---
+++ lib/players/computer.rb
@@ -0,0 +1,15 @@
+require "pry"
+
+class Player::Computer < Player
+  attr_accessor :board
+  WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+  ]
+  CORNERS = [0, 2, 8, 6]
