f256f3f1aa6e66dfd4543173b7e275fe71e25a98
Alyssa Brown <alyssa.brown@live.com>
2019-04-05 05:00:10 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,38 +1,15 @@
-class Board
-  attr_accessor :cells
-  def initialize
-    reset!
-  end
+require "pry"
 
-  def reset!
-    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  end
-  def display
-    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
-    puts "-----------"
-    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
-    puts "-----------"
-    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
-  end
-  def position(input)
-    input = input.to_i
-    cells[input - 1]
-  end
-  def full?
-    cells.none? {|cell| cell == " " || cell == nil}
-  end
-  def turn_count
-    cells.count {|cell| cell == "X" || cell == "O"}
-  end
-  def taken?(input)
-    position(input) == "X" || position(input) == "O"
-  end
-  def valid_move?(input)
-    input = input.to_i
-    !(taken?(input)) && input.between?(1, 9)
-  end
-  def update(input, player)
-    input = input.to_i
-    cells[input -1] = player.token
-  end
-end
+class Game
+  attr_accessor :board, :player_1, :player_2, :timer
+
+  WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+  ]
